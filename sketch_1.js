c6f34c295fee90f3bfb2eb163ffd78f803f32611
Hedda Lie <hedda.paulsen.lie@gmail.com>
2023-04-05 16:11:55 +0200

    added homework

---
+++ sketch_1.js
@@ -0,0 +1,39 @@
+let objectHeight = 200;
+let objectWidth = 200;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background("#d3d3d3");
+  
+  //base
+  fill("#fff");
+  rect(objectHeight - 70, objectWidth - 70, 130, 130);
+  
+  //screen
+  fill("#2d2d2d");
+  rect(objectHeight - 60, objectWidth - 60, 110, 20);
+  
+  //white buttons
+  fill("#fff");
+  square(objectHeight - 60, objectWidth - 30, 20);
+  square(objectHeight - 60, objectWidth, 20);
+  square(objectHeight - 60, objectWidth + 30, 20);
+  square(objectHeight - 30, objectWidth + 30, 20);
+  square(objectHeight - 30, objectWidth, 20);
+  square(objectHeight - 30, objectWidth - 30, 20);
+  square(objectHeight, objectWidth + 30, 20);
+  square(objectHeight, objectWidth, 20);
+  square(objectHeight, objectWidth - 30, 20);
+  
+  //black buttons
+  fill("#000");
+    square(objectHeight + 30, objectWidth + 30, 20);
+    square(objectHeight + 30, objectWidth, 20);
+    square(objectHeight + 30, objectWidth - 30, 20);
+ 
+  
+  
+}
