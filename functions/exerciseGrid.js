7b95f02a39eb42f8f647baeb00786f7d924daf08
Hedda Lie <hedda.paulsen.lie@gmail.com>
2023-05-22 22:31:04 +0200

    grid exercise from class may 17th

---
+++ functions/exerciseGrid.js
@@ -0,0 +1,13 @@
+let numX;
+let numY;
+
+function grid(x,y) {
+    console.log("das Ergebnis ist:")
+    return (x + y)/2;
+}
+
+//result = average(55,66);
+numX = grid (2,4);
+
+//console.log('result 1:', result);
+console.log('numY', numY);
